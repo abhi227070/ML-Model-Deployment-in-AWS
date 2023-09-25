047e590500af36e3a36652376d29e13f8d8db13c
Abhijeet <abhijeetmaharana77@gmail.com>
2023-09-25 21:21:17 +0530

    commit

--- application.py
+++ application.py
@@ -36,12 +36,6 @@ def predict():
         result = 'Not Suffered From diabetes.'
         
     return render_template('index.html',result = result)
-    
-    
-    
-    
-
-
 
 if __name__=='__main__':
     app.run(debug=True)
